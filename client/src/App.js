fb3d1e074d7e1e9f4c7cad0c9d66a9e1a03a3e33
rajib.khan <mrajibkhan@gmail.com>
2018-01-09 00:05:07 +1100

    added react client

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {catalogs: []}
+
+  componentDidMount() {
+    fetch('/catalogs')
+      .then(res => res.json())
+      .then(catalogs => this.setState({ catalogs }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">online shopping portal</h1>
+        </header>
+        <h2>Catalogs</h2>
+        {this.state.catalogs.map(catalog =>
+          <div key={catalog.catalogId}>{catalog.catalogName}</div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
